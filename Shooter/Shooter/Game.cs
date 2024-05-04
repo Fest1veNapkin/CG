97bff698f31029765a288dccbb5fb32a4d15455b
AI_ <arseniy.mironov.01@mail.ru>
2024-05-04 11:13:22 +0300

    Done!

--- Shooter/Shooter/Game.cs
+++ Shooter/Shooter/Game.cs
@@ -27,12 +27,22 @@ namespace Shooter
     // Game class that inherets from the Game Window Class
     internal class Game : GameWindow
     {
-        Block user;
+        // -- objects --
         BackGround background;
         BackGround win, lose;
-        Wall floor, wall;
-        List<Block> enemies = new List<Block>();
+
+        Wall user;
+        Stick stick;
+        Wall type_0, type_1;
+        List<Wall> objects = new List<Wall>();
         List<bool> was = new List<bool>();
+        int wall_count = 0;
+        // -- objects --
+
+        // angle and stick 
+
+        int space_type = 0;
+        double stick_angle = Math.PI / 4;
         int active = 0, counter = 1000;
         ShaderProgram program;
         // camera 
@@ -75,42 +85,47 @@ namespace Shooter
             base_y = -1f;  user_y = 0f;
             base_z = -22f; user_z = 0f;
             
-            user = new Block(new Vector3(base_x, base_y, base_z), "linux.jpg");
-            
-
-            floor = new Wall(new Vector3(0f, -3f, -22f), 60f, 1f, 36f, "windows.PNG");
+            // backgrounds
             background = new BackGround(new Vector3(0, 10, 0), "windows.PNG");
             lose = new BackGround(new Vector3(0, 0, -20), "defeat.jpg");
             win = new BackGround(new Vector3(0, 0, -20), "win.jpg");
-            wall = new Wall(new Vector3(-3f, 0.5f ,-22f), 2f, 6f, 10f, "android.PNG");
 
-            for (float i = -4f; i <= 4; i+=1f)
+            // user
+            user = new Wall(new Vector3(base_x, base_y, base_z), 1f, 1f, 1f, 0, "linux.jpg");
+            stick = new Stick(new Vector3(base_x+1, base_y+1, base_z), new Vector3(base_x + 5, base_y + 5, base_z), 10000f, "apple.jpg");
+            type_0 = new Wall(new Vector3(base_x-2, base_y + 10, base_z), 6f, 2f, 2f, 0, "stick.jpg");
+            type_1 = new Wall(new Vector3(base_x-2, base_y + 10, base_z), 6f, 2f, 2f, 0, "force.jpg");
+            // walls
+            objects.Add(new Wall(new Vector3(0f, -3f, -22f), 60f, 1f, 36f, 1, "windows.PNG"));
+            objects.Add(new Wall(new Vector3(-3f, 0.5f ,-22f), 2f, 6f, 10f,1 ,"android.PNG"));
+            wall_count = 2;
+
+            // create enemies
+            for (float i = -4f; i <= 4; i += 1f)
             {
-                for (float j = -4; j <= 4; j+=1)
+                for (float j = -4; j <= 4; j += 1)
                 {
                     float distance = (float)Math.Sqrt(i * i + j * j);
                     if (distance > 4) continue;
-                    Block enemy = new Block(new Vector3(11f + i, 2f + j, -22f), "apple.jpg");
-                    enemies.Add(enemy);
+                    Wall enemy = new Wall(new Vector3(11f + i, 2f + j, -22f), 1f, 1f, 1f, 2, "apple.jpg");
+                    objects.Add(enemy);
                     was.Add(true);
+
+
+                    if (distance <= 2)
+                    {
+                        enemy = new Wall(new Vector3(4f + i, 1f + j, -22f), 1f, 1f, 1f, 2, "compw.jpg");
+                        objects.Add(enemy);
+                        was.Add(true);
+                    }
                 }
             }
-            for (float i = -2f; i <= 0; i += 1f)
-            {
-                for (float j = 0; j <= 2; j += 1)
-                {
-                    float distance = (float)Math.Sqrt(i * i + j * j);
-                    if (distance > 2) continue;
-                    Block enemy = new Block(new Vector3(4f + i, 1f + j, -22f), "compm.jpg");
-                    enemies.Add(enemy);
-                    was.Add(true);
-                }
-            }
-            active = enemies.Count();
+
+            active = was.Count();
             program = new ShaderProgram("Default.vert", "Default.frag");
 
             GL.Enable(EnableCap.DepthTest);
-            camera = new Camera(width, height, Vector3.Zero);
+            camera = new Camera(width, height, new Vector3(0f, 3f, 3f));
             CursorState = CursorState.Grabbed;
             
         }
@@ -120,9 +135,10 @@ namespace Shooter
             base.OnUnload();
             background.Delete();
             user.Delete();
-            floor.Delete();
-            wall.Delete();
-            foreach (var enemy in enemies)
+            stick.Delete();
+            type_0.Delete();
+            type_1.Delete();
+            foreach (var enemy in objects)
             {
                 enemy.Delete();
             }
@@ -132,7 +148,6 @@ namespace Shooter
         // called every frame. All rendering happens here
         protected override void OnRenderFrame(FrameEventArgs args)
         {
-            
             // Set the color to fill the screen with
             GL.ClearColor(0f, 0f, 0f, 1f);
             // Fill the screen with the color
@@ -155,44 +170,57 @@ namespace Shooter
             GL.UniformMatrix4(projectionLocation, true, ref projection);
 
             
+            // -- game logic --
             background.Render(program);
-            floor.Render(program);
-            wall.Render(program);
-            for (int enemy = 0; enemy < enemies.Count; enemy++)
+            if (space_type != 1)
             {
-                if (was[enemy])
+                type_0.Render(program);
+            }
+            else type_1.Render(program);
+            // render objects
+            for (int wall = 0; wall < wall_count; ++wall)
+            {
+                objects[wall].Render(program);
+            }
+
+            for (int enemy = wall_count; enemy < objects.Count; enemy++)
+            {
+                if (was[enemy - wall_count])
                 {
-                    enemies[enemy].Render(program);
+                    objects[enemy].Render(program);
                 }
             }
+
             if (user_x <= -20f || user_x <= -20f || user_x > 50 || user_y > 50)
             {
                 reset();
             }
-            if (!hit_wall(floor) && v0 != 0)
-            { 
-                dt += 0.01f;
-                user_x = v0 * 0.5f * 2 * dt; 
-                user_y = v0 * 0.9f * 2* dt - 9.81f * dt * dt / 2f;
-            }
-            hit_wall(wall);
-            hit_block();
-            update();
 
+            bool hitted = hit();
+            
+            if (!hitted && v0 != 0 && space_type == 0)
+            { 
+                dt += 0.007f;
+                user_x = 2 * v0 * dt * (float)Math.Cos(stick_angle);
+                user_y = 2 * v0 * dt * (float)Math.Sin(stick_angle) - 0.5f * 9.81f * dt * dt;
+            }
+
+            update();
             model = Matrix4.Identity;
             Matrix4 translation = Matrix4.CreateTranslation(user_x + extra_x, user_y + extra_y, user_z);
             model *= translation;
+            
+            // -- game logic --
 
             modelLocation = GL.GetUniformLocation(program.ID, "model");
             viewLocation = GL.GetUniformLocation(program.ID, "view");
             projectionLocation = GL.GetUniformLocation(program.ID, "projection");
-
             GL.UniformMatrix4(modelLocation, true, ref model);
             GL.UniformMatrix4(viewLocation, true, ref view);
             GL.UniformMatrix4(projectionLocation, true, ref projection);
-
             user.Render(program);
-
+            stick.move(stick_angle);
+            stick.Render(program);
             // swap the buffers
             Context.SwapBuffers();
             base.OnRenderFrame(args);
@@ -223,43 +251,43 @@ namespace Shooter
             v0 = 0;
         }
 
-        protected bool hit_wall(Wall wall)
-        {
-            Vector3 user_pos = new Vector3(base_x + user_x + extra_x, base_y + user_y + extra_y, base_z + user_z + extra_z),
-                wall_pos = wall.position;
-            
-            if (Math.Abs(wall_pos.X -  user_pos.X) <= (0.5 + wall.len_x/2) &&
-                Math.Abs(wall_pos.Y - user_pos.Y) <= (0.5 + wall.len_y / 2) )
-            {
-                reset();
-                return true;
-            }
-            return false;
-        }
-
-        protected void hit_block()
+        protected bool hit()
         {
             Vector3 user_pos = new Vector3(base_x + user_x + extra_x, base_y + user_y + extra_y, base_z + user_z + extra_z);
+            bool res = false;
 
-            for (int enemy = 0; enemy < enemies.Count; enemy++)
+            // wall hit test
+            for (int wall = 0; wall < wall_count; ++wall)
             {
-                Block t = enemies[enemy];
-                if (was[enemy])
+                if (Math.Abs(objects[wall].position.X - user_pos.X) <= (0.5 + objects[wall].len_x / 2) &&
+                        Math.Abs(objects[wall].position.Y - user_pos.Y) <= (0.5 + objects[wall].len_y / 2))
                 {
-                    Vector3 block_pos = new Vector3(t.position);
+                    res = true;
+                    break;
+                }
+
+            }
+
+            // enemies hit test
+            for (int enemy = wall_count; enemy < objects.Count; enemy++)
+            {
+                if (was[enemy - wall_count] == true)
+                {
+                    Vector3 block_pos = new Vector3(objects[enemy].position);
                     float dist = Math.Abs(block_pos.X - user_pos.X) + Math.Abs(block_pos.Y - user_pos.Y) +
                         Math.Abs(block_pos.Z - user_pos.Z);
                     if (dist <= 2f)
                     {
-                        reset();
-                        was[enemy] = false;
+                        res = true;
+                        was[enemy - wall_count] = false;
                         active--;
                     }
                 }
             }
-
+            if (res) reset();
+            return res;
         }
-
+        
         protected void update()
         {
             KeyboardState input = KeyboardState;
@@ -269,71 +297,99 @@ namespace Shooter
                 need_update = !need_update;
             }
             if (!need_update) return;
-            
-            if (v0 < 1e-8)
+
+            if (input.IsKeyDown(Keys.Space))
+            { 
+                if ((space_type == 0 && v0 < 1e-8) || (space_type != 0))
+                {
+                    space_type++;
+                }
+            }
+            if (space_type == 0 && v0 < 1e-8)
             {
+                if (input.IsKeyDown(Keys.Up))
+                {
+                    if (stick_angle <= Math.PI / 2.5) stick_angle += 0.001f;
+                    
+                }
+                if (input.IsKeyDown(Keys.Down))
+                {
+                    if (stick_angle > Math.PI / 12) stick_angle -= 0.001f;
+                }
+            }
+            if (space_type == 1)
+            {
+                if (input.IsKeyDown(Keys.Up))
+                {
+                    v0 += 0.05f;
+                }
+                if (input.IsKeyDown(Keys.Down))
+                {
+                    v0 -= 0.05f;
+                }
+
                 if (input.IsKeyDown(Keys.D1))
                 {
-                    v0 = 1; counter--;
+                    v0 = 1;
                 }
                 if (input.IsKeyDown(Keys.D2))
                 {
-                    v0 = 2; counter--;
+                    v0 = 2;
                 }
                 if (input.IsKeyDown(Keys.D3))
                 {
-                    v0 = 3; counter--;
+                    v0 = 3;
                 }
                 if (input.IsKeyDown(Keys.D4))
                 {
-                    v0 = 4; counter--;
+                    v0 = 4;
                 }
                 if (input.IsKeyDown(Keys.D5))
                 {
-                    v0 = 5; counter--;
+                    v0 = 5;
                 }
                 if (input.IsKeyDown(Keys.D6))
                 {
-                    v0 = 6; counter--;
+                    v0 = 6;
                 }
                 if (input.IsKeyDown(Keys.D7))
                 {
-                    v0 = 7; counter--;
+                    v0 = 7;
                 }
                 if (input.IsKeyDown(Keys.D8))
                 {
-                    v0 = 8; counter--;
+                    v0 = 8;
                 }
                 if (input.IsKeyDown(Keys.D9))
                 {
-                    v0 = 9; counter--;
+                    v0 = 9;
                 }
                 if (input.IsKeyDown(Keys.D))
                 {
-                    //do something if Key is pressed
                     extra_x += 0.01f;
                 }
                 if (input.IsKeyDown(Keys.A))
                 {
-                    //do something if Key is pressed
                     extra_x -= 0.01f;
                 }
                 if (input.IsKeyDown(Keys.W))
                 {
-                    //do something if Key is pressed
                     extra_y += 0.01f;
                 }
                 if (input.IsKeyDown(Keys.S))
                 {
-                    //do something if Key is pressed
                     extra_y -= 0.01f;
                 }
-            }
 
-            if (input.IsKeyDown(Keys.Escape) || (((counter == 0) || (active == 0)) && (v0 < 1e-8)))
+            }
+            if (space_type == 2)
+            {
+                space_type = 0;
+            }
+            if (input.IsKeyDown(Keys.Escape) || (((counter == 0) || (active == 0)) && (v0 < 1e-8)) || (counter <= 0 || active <= 0))
             {
                 // -- stop --
-                if (active == 0 || counter == 0)
+                if (active <= 0 || counter <= 0)
                 {
                     int flag = 1000;
                     while (flag > 0)
@@ -357,7 +413,7 @@ namespace Shooter
                         GL.UniformMatrix4(viewLocation, true, ref view);
                         GL.UniformMatrix4(projectionLocation, true, ref projection);
                         
-                        if(active == 0)
+                        if(active <= 0)
                         {
                             win.Render(program);
                         }
--- Shooter/Shooter/Objects/Objects.cs
+++ Shooter/Shooter/Objects/Objects.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shooter.Graphics;
+using System.Drawing;
 
 namespace Shooter.Objects
 {
@@ -155,12 +156,18 @@ namespace Shooter.Objects
 
     }
 
-    internal class Block
+    internal class Wall
     {
         public Vector3 position;
         public List<Vector3> vertices = new List<Vector3>();
         public List<Vector2> uv = new List<Vector2>();
         public List<uint> ids;
+        /*
+         * type = 0 --> user
+         * type = 1 --> wall (user can`t touch it)
+         * type = 2 --> enemies (blocks which we need to destroy)
+         */
+        public int type;
 
         VAO vao;
         VBO vert_vbo;
@@ -168,11 +175,14 @@ namespace Shooter.Objects
         IBO ibo;
         Texture texture;
 
-
-        public Block(Vector3 position_, string texture_name)
+        public float len_x, len_y, len_z;
+        public Wall(Vector3 position_, float len_x_, float len_y_, float len_z_, int type_,  string texture_name)
         {
+            len_x = len_x_;
+            len_y = len_y_;
+            len_z = len_z_;
+            type = type_;
             position = position_;
-
             List<Vector3> raw_vertices = new List<Vector3>()
             {
                 new Vector3(-0.5f, 0.5f, 0.5f), // topleft vert
@@ -260,124 +270,6 @@ namespace Shooter.Objects
 
             foreach (var vert in raw_vertices)
             {
-                vertices.Add(vert + position);
-            }
-
-            vao = new VAO();
-            vert_vbo = new VBO(vertices);
-            ibo = new IBO(ids);
-            uv_vbo = new VBO(uv);
-            texture = new Texture(texture_name);
-            Build();
-        }
-
-        public void Build()
-        {
-
-            vao.Bind();
-            vert_vbo.Bind();
-
-            vao.LinkToVAO(0, 3, vert_vbo);
-
-            uv_vbo.Bind();
-            vao.LinkToVAO(1, 2, uv_vbo);
-        }
-        public void Render(ShaderProgram program) // drawing the chunk
-        {
-            program.Bind();
-            vao.Bind();
-            ibo.Bind();
-            texture.Bind();
-            GL.DrawElements(PrimitiveType.Triangles, ids.Count, DrawElementsType.UnsignedInt, 0);
-
-        }
-        public void Delete()
-        {
-            vao.Delete();
-            vert_vbo.Delete();
-            uv_vbo.Delete();
-            ibo.Delete();
-            texture.Delete();
-        }
-    }
-
-    internal class Wall
-    {
-        public Vector3 position;
-        public List<Vector3> vertices = new List<Vector3>();
-        public List<Vector2> uv = new List<Vector2>();
-        public List<uint> ids;
-
-        VAO vao;
-        VBO vert_vbo;
-        VBO uv_vbo;
-        IBO ibo;
-        Texture texture;
-
-        public float len_x, len_y, len_z;
-        public Wall(Vector3 position_, float len_x_, float len_y_, float len_z_, string texture_name)
-        {
-            len_x = len_x_;
-            len_y = len_y_;
-            len_z = len_z_;
-
-            position = position_;
-            uv = new List<Vector2>()
-            {
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-
-                new Vector2(0f, 1f),
-                new Vector2(1f, 1f),
-                new Vector2(1f, 0f),
-                new Vector2(0f, 0f),
-            };
-            ids = new List<uint>()
-            {
-                0, 1, 2,
-                2, 3, 0,
-
-                4, 5, 6,
-                6, 7, 4,
-
-                8, 9, 10,
-                10, 11, 8,
-
-                12, 13, 14,
-                14, 15, 12,
-
-                16, 17, 18,
-                18, 19, 16,
-
-                20, 21, 22,
-                22, 23, 20
-            };
-            Block block = new Objects.Block(new Vector3(0, 0, 0), texture_name);
-
-            foreach (var vert in block.vertices)
-            {
                 Vector3 dif = new Vector3(len_x/ (2f), len_y/ (2f), len_z/ (2f));
                 if (vert.X < 0)
                 {
@@ -429,6 +321,93 @@ namespace Shooter.Objects
             uv_vbo.Delete();
             ibo.Delete();
             texture.Delete();
+        }
+    }
+
+    internal class Stick
+    {
+        public Vector3 position;
+        public List<Vector3> vertices = new List<Vector3>();
+        Vector3 bas = new Vector3();
+        public List<Vector2> uv = new List<Vector2>();
+        public List<uint> ids;
+        /*
+         * type = 0 --> user
+         * type = 1 --> wall (user can`t touch it)
+         * type = 2 --> enemies (blocks which we need to destroy)
+         */
+        public int type;
+        double angle = Math.PI / 4;
+        VAO vao;
+        VBO vert_vbo;
+        VBO uv_vbo;
+        IBO ibo;
+        Texture texture;
+        float Thickness;
+        double len;
+
+        public float len_x, len_y, len_z;
+        public Stick(Vector3 position_, Vector3 position__, float thickness, string texture_name)
+        {
+            
+            uv = new List<Vector2>()
+            {
+                new Vector2(0f, 0f),
+                new Vector2(1f, 1f),
+            };
+            ids = new List<uint>()
+            {
+                0, 1
+            };
+            Thickness = thickness;
+            vertices.Add(position_);
+            vertices.Add(position__);
+            bas = new Vector3(vertices[1]);
+            vao = new VAO();
+            ibo = new IBO(ids);
+            uv_vbo = new VBO(uv);
+            texture = new Texture(texture_name);
+            len = Math.Sqrt((position_.X - position__.X) * (position_.X - position__.X)
+                * (position_.Y - position__.Y) * (position_.Y - position__.Y)
+                * (position_.Z - position__.Z) * (position_.Z - position__.Z));
+            Build();
+        }
+        public void Build()
+        {
+
+            vao.Bind();
+            vert_vbo = new VBO(vertices);
+            vert_vbo.Bind();
+
+            vao.LinkToVAO(0, 3, vert_vbo);
+
+            uv_vbo.Bind();
+            vao.LinkToVAO(1, 2, uv_vbo);
+        }
+        public void Render(ShaderProgram program) // drawing the chunk
+        {
+            program.Bind();
+            Build();
+            vao.Bind();
+            ibo.Bind();
+            texture.Bind();  
+            GL.DrawElements(PrimitiveType.Lines, ids.Count, DrawElementsType.UnsignedInt, 0);
+  
+
+        }
+        public void Delete()
+        {
+            vao.Delete();
+            vert_vbo.Delete();
+            uv_vbo.Delete();
+            ibo.Delete();
+            texture.Delete();
+        }
+
+        public void move(double new_angle)
+        {
+            angle = new_angle;
+            vertices[1] = new Vector3(bas.X, (float)Math.Tan(angle) * bas.Y, bas.Z);
         }
     }
 }
